6c93efcd89ab6c3ae42c7dec1b3f4ae95d4773b8
deepak429 <deepakch429@gmail.com>
2025-01-18 20:33:11 +0530

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during builds
   },
-  images:{
-    domains:[
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript checks during build
+  },
+  images: {
+    domains: [
       "res.cloudinary.com"
-    ]
-  }
+    ],
+  },
 };
 
 export default nextConfig;
