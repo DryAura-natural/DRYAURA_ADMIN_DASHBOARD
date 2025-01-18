fd327079b392ce75005fc6a1f799e0361120e633
deepak429 <deepakch429@gmail.com>
2025-01-18 19:51:13 +0530

    some changes

--- app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,19 +1,34 @@
-import prismadb from "@/lib/prismadb";
+import { PrismaClient, Billboard } from "@prisma/client";
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = async({params}:{ params:{billboardId:string} }) =>{
-      const billboard = await prismadb.billboard.findUnique({
-            where:{
-                  id:params.billboardId
-            }
-      })
-     
-      return(
-            <div className="flex-col ">
-                  <div className="flex-1 space-y-4 p-8 pt-6">
-                        <BillboardForm initialData={billboard}/>
-                  </div>
-            </div>
-      )
+const prismadb = new PrismaClient();
+
+interface BillboardPageProps {
+  params: {
+    billboardId: string;
+  };
 }
+
+const BillboardPage = async ({ params }: BillboardPageProps) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+
+  // Null check for the billboard
+  if (!billboard) {
+    return <div>Billboard not found</div>;
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        {/* Make sure BillboardForm expects a `billboard` type as initialData */}
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  );
+};
+
 export default BillboardPage;
